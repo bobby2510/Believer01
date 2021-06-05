a90f17911af485179259215807c829422800ecbd
bobby2510 <65103925+bobby2510@users.noreply.github.com>
2021-06-05 14:56:40 +0530

    Update mode.js

--- static/js/mode.js
+++ static/js/mode.js
@@ -45,6 +45,6 @@ let mode_method = function(team1,team2,mn,index_one,index_two,series_index,mode,
         team_generator(index_one,index_two,team1,team2,mn,98,100,[],[],c_one_index,c_two_index,vc_one_index,vc_two_index,selected_tsd,series_index,mode,sport_id,fantasy)
     }
     else{
-        team_generator(index_one,index_two,team1,team2,mn,94,100,[],[],index_one,index_two,index_one,index_two,selected_tsd,series_index,mode,sport_id,fantasy)
+        team_generator(index_one,index_two,team1,team2,mn,97,100,[],[],c_one_index,c_two_index,vc_one_index,vc_two_index,selected_tsd,series_index,mode,sport_id,fantasy)
     }
 }
