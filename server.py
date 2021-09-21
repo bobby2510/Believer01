a63ce1c7a3dd9fef4282997bea1309ed28f8b66a
bobby2510 <65103925+bobby2510@users.noreply.github.com>
2021-09-21 16:08:52 +0530

    Update server.py

--- server.py
+++ server.py
@@ -6,5 +6,9 @@ app = flask.Flask(__name__,
 def home():
     return flask.render_template('start.html')
 
+@app.route('/winning_result',methods=['GET',])
+def winning():
+    return flask.render_template('result.html')
+
 if __name__ == '__main__':
     app.run()
